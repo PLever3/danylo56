715a0be76c8ada54627aac2c7dc7f6e6820bf43e
Danylo Zakharchenko <danyko.zakharchenko@gmail.com>
2018-07-10 19:47:12 +0300

    ex_2

---
+++ python/ex _2.py
@@ -0,0 +1,26 @@
+import random
+a = random.randint(0, 10)
+c = int(input('c = :'))
+if(a == c):
+    print('OK')
+break
+elif(a < c):
+    print('smaller')
+elif(a > c):
+    print('biger')
+c = int(input('c = :'))
+if(a == c):
+    print('OK')
+break
+elif(a < c):
+    print('smaller')
+elif(a > c):
+    print('biger')
+c = int(input('c = :'))
+if(a == c):
+    print('OK')
+elif(a != c):
+    print(a)
+
+
+    
