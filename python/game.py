051575f4adcc545ffc8e6b5423215164bf639103
Danylo Zakharchenko <danyko.zakharchenko@gmail.com>
2018-07-11 13:20:32 +0300

    game

--- python/game.py
+++ python/game.py
@@ -1,12 +1,103 @@
-import random
-a = random.randint(10000, 99999)
-print('1 num =', int((a%100000)/10000))
-print('2 num =', int((a%10000)/1000))
-print('3 num =', int((a%1000)/100))
-print('4 num =', int((a%100)/10))
-print('1 num =', int((a%10)))
-c = int(input('введите число :'))
-if(a == c):
-    print('OK')
-else:
-    print('ERROR')
+import time
+print(' Menu '
+          '\n1 - пятизначные ',
+          '\n2 - шестизначные ',
+          '\n3 - семизначные',
+          '\n4 - восьмизначные',
+          )
+operation = input('Choose youroperation: ')
+print (operation)
+if operation == '1' :
+        import random
+        a = random.randint(10000, 99999)
+        print('1 num =', int((a%100000)/10000))
+        time.sleep(3)
+        print('2 num =', int((a%10000)/1000))
+        time.sleep(3)
+        print('3 num =', int((a%1000)/100))
+        time.sleep(3)
+        print('4 num =', int((a%100)/10))
+        time.sleep(3)
+        print('1 num =', int((a%10)))
+        time.sleep(3)
+        c = int(input('введите число :'))
+        if(a == c):
+            print('CORRECT')
+        else:
+            print('INCORRECT')
+elif operation == '2':
+        import random
+        a = random.randint(100000, 999999)
+        print('1 num =', int((a%1000000)/100000))
+        time.sleep(3)
+        print('2 num =', int((a%100000)/10000))
+        time.sleep(3)
+        print('3 num =', int((a%10000)/1000))
+        time.sleep(3)
+        print('4 num =', int((a%1000)/100))
+        time.sleep(3)
+        print('5 num =', int((a%100)/10))
+        time.sleep(3)
+        print('6 num =', int((a%10)))
+        time.sleep(3)
+        c = int(input('введите число :'))
+        if(a == c):
+            print('CORRECT')
+        else:
+            print('INCORRECT')
+elif operation == '3':
+        import random
+        a = random.randint(1000000, 9999999)
+        print('1 num =', int((a%10000000)/1000000))
+        time.sleep(3)
+        print('2 num =', int((a%1000000)/100000))
+        time.sleep(3)
+        print('3 num =', int((a%100000)/10000))
+        time.sleep(3)
+        print('4 num =', int((a%10000)/1000))
+        time.sleep(3)
+        print('5 num =', int((a%1000)/100))
+        time.sleep(3)
+        print('6 num =', int((a%100)/10))
+        time.sleep(3)
+        print('7 num =', int((a%10)))
+        time.sleep(3)
+        c = int(input('введите число :'))
+        if(a == c):
+            print('CORRECT')
+        else:
+            print('INCORRECT')
+elif operation == '4':
+        import random
+        a = random.randint(10000000, 99999999)
+        print('1 num =', int((a%100000000)/10000000))
+        time.sleep(3)
+        print('2 num =', int((a%10000000)/1000000))
+        time.sleep(3)
+        print('3 num =', int((a%1000000)/100000))
+        time.sleep(3)
+        print('4 num =', int((a%100000)/10000))
+        time.sleep(3)
+        print('5 num =', int((a%10000)/1000))
+        time.sleep(3)
+        print('6 num =', int((a%1000)/100))
+        time.sleep(3)
+        print('7 num =', int((a%100)/10))
+        time.sleep(3)
+        print('8 num =', int((a%10)))
+        time.sleep(3)
+        c = int(input('введите число :'))
+        if(a == c):
+            print('CORRECT')
+        else:
+            print('INCORRECT')
+        
+  
+        
+  
+    
+    
+        
+
+    
+
